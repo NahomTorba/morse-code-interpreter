46f0476accf9424c9468aab6e637a98f99cad504
Nahom <nahumtorba@gmail.com>
2025-04-06 16:31:17 +0300

    initial text to morse

---
+++ MorseCode.py
@@ -0,0 +1,22 @@
+# Morse Code Dictionary
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+    ' ': '/'
+}
+
+def text_to_morse(text):
+    text = text.upper()
+    morse_code = ' '.join(morse_code_dict.get(char, '') for char in text)
+    return morse_code
+
+# Input text from the user
+user_input = input("Enter text to convert to Morse code: ")
+morse_output = text_to_morse(user_input)
+
+print(f"Message: {morse_output}")
